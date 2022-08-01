441361d017f5d98c18c3d6b32e43c7de737ed7ca
Plagiatus <lukas.scheuerle@web.de>
2022-08-01 21:12:00 +0300

    add footer

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,9 +1,10 @@
 <script lang="ts">
 import Navigation from './components/Navigation.vue';
+import Footer from './components/Footer.vue';
 import { defineComponent } from "vue";
 
 export default defineComponent({
-	components: { Navigation },
+	components: { Navigation, Footer },
 })
 </script>
 
@@ -12,10 +13,12 @@ export default defineComponent({
 	<div id="main-view">
 		<router-view></router-view>
 	</div>
+	<Footer />
 </template>
 
 <style>
 #main-view {
 	padding: 1em;
+	margin-bottom: 2rem;
 }
 </style>
---
+++ website/src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+<footer>
+    Made with ♥ by <a href="https://plagiatus.net" target="_blank" rel="noopener noreferrer">Plagiatus</a>
+</footer>
+</template>
+
+
+<style> 
+footer {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    background-color: var(--background2);
+    padding: .1em .5em;    
+}
+
+
+</style>
