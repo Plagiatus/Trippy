48bfdd5a4c1323366d0d1f44b23e78eb38d8696d
Plagiatus <lukas.scheuerle@web.de>
2022-09-06 19:39:34 +0300

    better session page

--- website/src/views/Sessions.vue
+++ website/src/views/Sessions.vue
@@ -1,10 +1,12 @@
 <template>
-	<div>
+	<div id="sessions-wrapper">
 		<router-link class="sessions-link" :to="{ name: 'Sessions.Setup' }">
-			<h2>&rarr; set up a new session</h2>
+			<img src="@/assets/add.svg" alt="New Session">
+			<span>Set up a new session</span>
 		</router-link>
 		<router-link class="sessions-link" :to="{ name: 'Sessions.Feedback' }">
-			<h2>&rarr; view feedback</h2>
+			<img src="@/assets/user-feedback.svg" alt="Show feedback">
+			<span>View feedback</span>
 		</router-link>
 	</div>
 </template>
@@ -13,5 +15,35 @@
 .sessions-link {
 	text-decoration: none;
 	color: var(--text-color);
+	margin: 2rem;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.sessions-link > img {
+	filter: var(--text-color-filter);
+	width: 100%;
+	width: 200px;
+}
+
+.sessions-link > span {
+	font-size: 2em;
+	font-weight: bold;
+}
+
+#sessions-wrapper {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-evenly;
+}
+
+@media screen and (max-width: 740px) {
+	#sessions-wrapper {
+		flex-direction: column;
+	}
+	.sessions-link > span {
+		font-size: 1.5em;
+	}
 }
 </style>
