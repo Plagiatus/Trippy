3fc41f74f29c078c5f73fa064a54f99ccf9d2690
Plagiatus <lukas.scheuerle@web.de>
2022-09-22 20:17:36 +0300

    add stricter constraints to Config

--- bot/src/validator/constraints.ts
+++ bot/src/validator/constraints.ts
@@ -1,19 +1,20 @@
 export const Constraints = {
 	Config: {
-		botToken: { presence: true,},
-		serverId: { presence: true,},
-		appId: { presence: true,},
+		botToken: { presence: true, length: {minimum: 10}},
+		serverId: { presence: true, length: {minimum: 18}},
+		appId: { presence: true, length: {minimum: 18}},
+		port: { presence: true, numericality: {onlyInteger: true, noStrings: true}},
 		// "db.user": { presence: true },
 		// "db.password": { presence: true },
 		"db.url": { presence: true },
 		"db.name": { presence: true },
 
-		"channels.modLog": { presence: true },
-		"channels.systemLog": { presence: true },
-		"channels.sessionList": { presence: true },
-		"channels.activeSessions": { presence: true },
+		"channels.modLog": { presence: true, length: {minimum: 18} },
+		"channels.systemLog": { presence: true, length: {minimum: 18} },
+		"channels.sessionList": { presence: true, length: {minimum: 18} },
+		"channels.activeSessions": { presence: true, length: {minimum: 18} },
 
-		"roles.mods": { presence: true },
-		"roles.hosts": { presence: true },
+		"roles.mods": { presence: true, length: {minimum: 18} },
+		"roles.hosts": { presence: true, length: {minimum: 18} },
 	},
 }
