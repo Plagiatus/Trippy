664d88b0596f341ed79bac968b7b8df66b227516
Plagiatus <lukas.scheuerle@web.de>
2023-05-22 20:57:57 +0300

    fix isAlphaNumerics weird var declarations

--- bot/src/utils/validation-utils.ts
+++ bot/src/utils/validation-utils.ts
@@ -1,9 +1,7 @@
 class ValidationUtils {
-	isAlphaNumeric(str: string) {
-		let code, i, len;
-		
-		for (i = 0, len = str.length; i < len; i++) {
-			code = str.charCodeAt(i);
+	isAlphaNumeric(str: string): boolean {
+		for (let i = 0; i < str.length; i++) {
+			let code = str.charCodeAt(i);
 			if (!(code > 47 && code < 58) && // numeric (0-9)
 					!(code > 64 && code < 91) && // upper alpha (A-Z)
 					!(code > 96 && code < 123)) { // lower alpha (a-z)
